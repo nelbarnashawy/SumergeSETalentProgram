c90e347f75a7e2c051772e19346d646999263ec6
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 18:10:12 +0300

    Created courseImp6 variable to prioritize by variable name in constructor

--- src/main/java/com/sumerge/dependencyinjectiontask/CourseService.java
+++ src/main/java/com/sumerge/dependencyinjectiontask/CourseService.java
@@ -8,10 +8,12 @@ import org.springframework.stereotype.Service;
 public class CourseService {
     private final CourseRecommender courseRecommender;
     private final CourseRecommender2 courseRecommender2;
+    private final CourseRecommender3 courseImp6;
 
     @Autowired
-    public CourseService(CourseRecommender courseRecommender, @Qualifier("courseImp3") CourseRecommender2 courseRecommender2) {
+    public CourseService(CourseRecommender courseRecommender, @Qualifier("courseImp3") CourseRecommender2 courseRecommender2, CourseRecommender3 courseImp6) {
         this.courseRecommender = courseRecommender;
         this.courseRecommender2 = courseRecommender2;
+        this.courseImp6=courseImp6;
     }
 }
