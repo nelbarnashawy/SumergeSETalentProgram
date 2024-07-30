814bf772fd915bfae59313314a4a5b33b5a43bf3
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 18:03:55 +0300

    Added new CourseRecommender2 variable and Added @Qualifier to courseImp3 in the constructor, to prioritize courseImp3 when constructing

--- src/main/java/com/sumerge/dependencyinjectiontask/CourseService.java
+++ src/main/java/com/sumerge/dependencyinjectiontask/CourseService.java
@@ -1,14 +1,17 @@
 package com.sumerge.dependencyinjectiontask;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
     private final CourseRecommender courseRecommender;
+    private final CourseRecommender2 courseRecommender2;
 
     @Autowired
-    public CourseService(CourseRecommender courseRecommender) {
+    public CourseService(CourseRecommender courseRecommender, @Qualifier("courseImp3") CourseRecommender2 courseRecommender2) {
         this.courseRecommender = courseRecommender;
+        this.courseRecommender2 = courseRecommender2;
     }
 }
