22bf8476b4efe5690c91118bcebedfa6219f73f7
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 18:27:53 +0300

    Added main method with run argument

--- src/main/java/com/sumerge/dependencyinjectiontask/SpringTaskApplication.java
+++ src/main/java/com/sumerge/dependencyinjectiontask/SpringTaskApplication.java
@@ -1,4 +1,4 @@
-package com.example.springTask;
+package com.sumerge.dependencyinjectiontask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class SpringTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTaskApplication.class, args);
-	}
 
+
+	}
 }
