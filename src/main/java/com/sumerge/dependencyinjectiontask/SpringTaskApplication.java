2f2038ee08163d0d15d962b44ab12f77948aeeef
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 18:44:21 +0300

    Added ApplicationContext to test app
    Test Successful.

--- src/main/java/com/sumerge/dependencyinjectiontask/SpringTaskApplication.java
+++ src/main/java/com/sumerge/dependencyinjectiontask/SpringTaskApplication.java
@@ -2,13 +2,17 @@ package com.sumerge.dependencyinjectiontask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringTaskApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringTaskApplication.class, args);
-
+		ApplicationContext context = SpringApplication.run(SpringTaskApplication.class, args);
+//		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		CourseService service = context.getBean(CourseService.class);
+		service.recommendCourse();
 
 	}
+
 }
