2ebb449a02514a3ed580d9d7055065039acb4596
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 17:48:57 +0300

    Added @Primary to set CourseImplementation1 as the primary bean

--- src/main/java/com/sumerge/dependencyinjectiontask/CourseImplementation1.java
+++ src/main/java/com/sumerge/dependencyinjectiontask/CourseImplementation1.java
@@ -1,7 +1,9 @@
 package com.sumerge.dependencyinjectiontask;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component("courseImp1")
 public class CourseImplementation1 implements CourseRecommender{
 
