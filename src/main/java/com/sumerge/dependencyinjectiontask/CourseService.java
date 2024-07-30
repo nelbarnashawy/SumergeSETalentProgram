5cc9c55956da9b072643e1a6553f16a9d1b96263
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-30 17:36:30 +0300

    Created variable courseRecommender and Autowired injection by construction

--- src/main/java/com/sumerge/dependencyinjectiontask/CourseService.java
+++ src/main/java/com/sumerge/dependencyinjectiontask/CourseService.java
@@ -1,9 +1,14 @@
 package com.sumerge.dependencyinjectiontask;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class CourseService {
+    private final CourseRecommender courseRecommender;
 
-
+    @Autowired
+    public CourseService(CourseRecommender courseRecommender) {
+        this.courseRecommender = courseRecommender;
+    }
 }
