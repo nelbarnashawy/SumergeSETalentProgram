67c66e9b60276b55a1693e07b767d5b3b2c9880b
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-31 19:52:41 +0300

    Fixed findAll by using RowMapper to map each row in the list and get the entries.

--- src/main/java/com/sumerge/springtask/repository/CourseRepositoryImp.java
+++ src/main/java/com/sumerge/springtask/repository/CourseRepositoryImp.java
@@ -4,8 +4,11 @@ import com.sumerge.springtask.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +36,18 @@ public class CourseRepositoryImp implements CourseRepository {
     @Override
     public List<Course> findAll() {
         String selectQuery = "select * from course";
-        ArrayList<Course> courses = (ArrayList<Course>) template.query(selectQuery, new BeanPropertyRowMapper<Course>(Course.class));
+        RowMapper<Course> mapper = new RowMapper<Course>() {
+            @Override
+            public Course mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Course c = new Course();
+                c.setCourse_id(rs.getInt(1));
+                c.setCourse_name(rs.getString(2));
+                c.setCourse_description(rs.getString(3));
+                c.setCourse_credit(rs.getInt(4));
+                return c;
+            }
+        };
+        List<Course> courses = template.query(selectQuery, mapper);
         return courses;
     }
 
