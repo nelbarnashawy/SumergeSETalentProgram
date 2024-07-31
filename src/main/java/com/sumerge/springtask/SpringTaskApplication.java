3216166dff1c297e477059bf3a74bd3029df9f3e
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-31 18:44:42 +0300

    Removed a comment from SpringTaskApplication

--- src/main/java/com/sumerge/springtask/SpringTaskApplication.java
+++ src/main/java/com/sumerge/springtask/SpringTaskApplication.java
@@ -9,9 +9,8 @@ public class SpringTaskApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringTaskApplication.class, args);
-//		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		CourseService service = context.getBean(CourseService.class);
-		service.recommendCourse();
+
 
 	}
 
