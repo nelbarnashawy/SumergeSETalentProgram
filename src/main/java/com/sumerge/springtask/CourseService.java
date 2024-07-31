0cb94b7f1ddd2ddc50699a85026afa7f8139ee9f
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-31 19:24:33 +0300

    Implemented courseRepo in CourseService and setup the Autowired to setter of CourseRepository and setter of CourseRecommender

--- src/main/java/com/sumerge/springtask/CourseService.java
+++ src/main/java/com/sumerge/springtask/CourseService.java
@@ -1,20 +1,63 @@
 package com.sumerge.springtask;
 
+import com.sumerge.springtask.model.Course;
+import com.sumerge.springtask.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CourseService {
+import java.util.List;
 
-    private final CourseRecommender courseRecommender;
+@Service
+public class CourseService implements CourseRepository{
+
+    private CourseRepository courseRepository;
+    private CourseRecommender courseRecommender;
+
+    public CourseRepository getCourseRepository() {
+        return courseRepository;
+    }
 
     @Autowired
-    public CourseService(CourseRecommender courseRecommender) {
-        this.courseRecommender = courseRecommender;
+    public void setCourseRepository(@Qualifier("courseRepo") CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 
+    public CourseRecommender getCourseRecommender() {
+        return courseRecommender;
+    }
+
+    @Autowired
+    public void setCourseRecommender(CourseRecommender courseRecommender) {
+        this.courseRecommender = courseRecommender;
     }
 
     public void recommendCourse(){
         System.out.println(courseRecommender.recommendCourse());
+    }
+
+    @Override
+    public Course findById(int id) {
+        return courseRepository.findById(id);
+    }
+
+    @Override
+    public List<Course> findAll() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public void save(Course course) {
+        courseRepository.save(course);
+    }
+
+    @Override
+    public void delete(Course course) {
+        courseRepository.delete(course);
+    }
+
+    @Override
+    public void update(Course course) {
+        courseRepository.update(course);
     }
 }
