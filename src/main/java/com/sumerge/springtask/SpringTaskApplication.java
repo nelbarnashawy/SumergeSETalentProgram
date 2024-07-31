2226ded6766238d152cfe6f527eb48b112bd35cb
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-31 20:25:26 +0300

    Successfully tested the findAll method using CommandLineRunner

--- src/main/java/com/sumerge/springtask/SpringTaskApplication.java
+++ src/main/java/com/sumerge/springtask/SpringTaskApplication.java
@@ -13,17 +13,20 @@ import java.util.ArrayList;
 public class SpringTaskApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(SpringTaskApplication.class, args);
-		CourseService service = context.getBean(CourseService.class);
+//		ApplicationContext context =
+		SpringApplication.run(SpringTaskApplication.class, args);
+//		CourseService service = context.getBean(CourseService.class);
 
-		Course course1 = context.getBean(Course.class);
+//		Course course1 = context.getBean(Course.class);
 
-		course1.setCourse_name("Math");
-		course1.setCourse_description("Math101 Course");
-		course1.setCourse_credit(8);
+//		course1.setCourse_name("Math");
+//		course1.setCourse_description("Math101 Course");
+//		course1.setCourse_credit(8);
 
-		service.save(course1);
-		System.out.println(service.findAll());
+//		service.delete(service.findById(1));
+
+		//service.save(course1);
+//		System.out.println(service.findAll());
 
 
 	}
