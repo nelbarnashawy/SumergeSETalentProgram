360dd68e118a0e654d9619ad2144d87dfaad7de1
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-31 19:47:57 +0300

    Added test data

--- src/main/java/com/sumerge/springtask/SpringTaskApplication.java
+++ src/main/java/com/sumerge/springtask/SpringTaskApplication.java
@@ -1,8 +1,13 @@
 package com.sumerge.springtask;
 
+import com.sumerge.springtask.model.Course;
+import com.sumerge.springtask.repository.CourseRepository;
+import com.sumerge.springtask.service.CourseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringTaskApplication {
@@ -10,6 +15,15 @@ public class SpringTaskApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringTaskApplication.class, args);
 		CourseService service = context.getBean(CourseService.class);
+
+		Course course1 = context.getBean(Course.class);
+
+		course1.setCourse_name("Math");
+		course1.setCourse_description("Math101 Course");
+		course1.setCourse_credit(8);
+
+		service.save(course1);
+		System.out.println(service.findAll());
 
 
 	}
