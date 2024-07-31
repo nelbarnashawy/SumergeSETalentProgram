d07d2b4947fbd6e217dd3484a49a7418b21b6d84
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-07-31 18:38:39 +0300

    Create Course.java file to contain course info and getters and setters

---
+++ src/main/java/com/sumerge/springtask/model/Course.java
@@ -0,0 +1,48 @@
+package com.sumerge.springtask.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Course {
+
+
+    private int course_id;
+    private String course_name;
+    private String course_description;
+    private int course_credit;
+
+
+    public int getCourse_id() {
+        return course_id;
+    }
+
+    public void setCourse_id(int course_id) {
+        this.course_id = course_id;
+    }
+
+    public int getCourse_credit() {
+        return course_credit;
+    }
+
+    public void setCourse_credit(int course_credit) {
+        this.course_credit = course_credit;
+    }
+
+    public String getCourse_description() {
+        return course_description;
+    }
+
+    public void setCourse_description(String course_description) {
+        this.course_description = course_description;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+}
