c9d7c032e678ec60bc75e659ac62456f4a6d7c1e
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-04 14:48:48 +0300

    Added courseImplementation2 as an imported bean

--- src/main/java/com/sumerge/dependencyinjectiontask/configuration/CourseRecommenderConfig.java
+++ src/main/java/com/sumerge/dependencyinjectiontask/configuration/CourseRecommenderConfig.java
@@ -1,29 +1,25 @@
 package com.sumerge.dependencyinjectiontask.configuration;
 
 import com.sumerge.dependencyinjectiontask.CourseImplementation1;
-import com.sumerge.dependencyinjectiontask.CourseImplementation2;
 import com.sumerge.dependencyinjectiontask.CourseImplementation3;
 import com.sumerge.dependencyinjectiontask.CourseRecommender;
 import com.sumerge.dependencyinjectiontask.service.CourseService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "org.example")
 public class CourseRecommenderConfig {
 
     @Bean
-    public CourseService courseService(@Qualifier("courseImplementation3") CourseRecommender courseRecommender) {
+    public CourseService courseService(@Qualifier("courseImplementation2") CourseRecommender courseRecommender) {
         return new CourseService(courseRecommender);
     }
     @Bean
     public CourseRecommender courseImplementation1() {
         return new CourseImplementation1();
-    }
-
-    @Bean
-    public CourseRecommender courseImplementation2() {
-        return new CourseImplementation2();
     }
 
     @Bean
