4bcbb127b15db1227b582df40f17160a97d3bde8
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-04 12:57:31 +0300

    Used AnnotationConfigApplicationContext instead of SpringApplication to run context, to avoid usage of Spring Boot

--- src/main/java/com/sumerge/dependencyinjectiontask/SpringTaskApplication.java
+++ src/main/java/com/sumerge/dependencyinjectiontask/SpringTaskApplication.java
@@ -1,15 +1,17 @@
 package com.sumerge.dependencyinjectiontask;
 
 
+import com.sumerge.dependencyinjectiontask.configuration.CourseRecommenderConfig;
+import com.sumerge.dependencyinjectiontask.service.CourseService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringTaskApplication {
 
 	public static void main(String[] args) {
-		//ApplicationContext context = SpringApplication.run(SpringTaskApplication.class, args);
-//		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-		//CourseService service = context.getBean(CourseService.class);
-		//service.recommendCourse();
+		ApplicationContext context = new AnnotationConfigApplicationContext(CourseRecommenderConfig.class);
+		CourseService service = context.getBean(CourseService.class);
+		service.recommendCourse();
 
 	}
 
