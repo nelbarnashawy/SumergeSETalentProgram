3fd81a573d4dac0cbcfeba4cf0655d494669931f
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-04 18:46:05 +0300

    Created Controller mappings for GET, POST, PUT, & DELETE

--- src/main/java/com/sumerge/springtask/controller/CourseController.java
+++ src/main/java/com/sumerge/springtask/controller/CourseController.java
@@ -1,20 +1,55 @@
 package com.sumerge.springtask.controller;
 
-import com.sumerge.springtask.repository.CourseRepositoryImp;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sumerge.springtask.model.Course;
+import com.sumerge.springtask.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 public class CourseController {
+
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping("/")
     public String home() {
         return "Hello User";
     }
 
-    @GetMapping("/recommendedCourses")
+    @GetMapping("/view/{id}")
+    public String viewById(@PathVariable int id) {
+        return courseService.findById(id).toString();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/add")
+    public void add(@RequestBody Course course) {
+        courseService.save(course);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/update/{id}")
+    public void update(@PathVariable int id, @RequestBody Course updatedCourse) {
+        courseService.update(id, updatedCourse);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable int id, @RequestBody Course course) {
+        courseService.delete(id, course);
+    }
+
+    @GetMapping("/discover")
     public String recommendedCourses() {
-        return new CourseRepositoryImp().findAll().toString();
+        List<Course> courses = courseService.recommendCourse();
+        return courses.toString();
     }
 }
