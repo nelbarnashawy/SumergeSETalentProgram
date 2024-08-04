a1436c7c24543625fe781026f505e5f5b17ebf7f
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-04 17:14:27 +0300

    Added a working Home Page to greet user

--- src/main/java/com/sumerge/springtask/controller/CourseController.java
+++ src/main/java/com/sumerge/springtask/controller/CourseController.java
@@ -1,13 +1,19 @@
 package com.sumerge.springtask.controller;
 
 import com.sumerge.springtask.repository.CourseRepositoryImp;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
 
-    @RequestMapping("/recommendedCourses")
+    @GetMapping("/")
+    public String home() {
+        return "Hello User";
+    }
+
+    @GetMapping("/recommendedCourses")
     public String recommendedCourses() {
         return new CourseRepositoryImp().findAll().toString();
     }
