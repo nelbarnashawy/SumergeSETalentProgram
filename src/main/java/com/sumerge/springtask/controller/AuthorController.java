b0f6e5801e2f70e5bb920bd667827c8045ae2b97
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-06 17:10:48 +0300

    Created AuthorController

---
+++ src/main/java/com/sumerge/springtask/controller/AuthorController.java
@@ -0,0 +1,30 @@
+package com.sumerge.springtask.controller;
+
+import com.sumerge.springtask.DTOs.AuthorDTO;
+import com.sumerge.springtask.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+
+    private AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Authors page";
+    }
+
+    @GetMapping("/authorByEmail/{email}")
+    public AuthorDTO authorByEmail(@PathVariable String email) {
+        return authorService.findAuthorByEmail(email);
+    }
+
+}
+
