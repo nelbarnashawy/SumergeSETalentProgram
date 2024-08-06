8176c5b1dd4a0960e166d0be4ab35677d496e3be
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-06 17:10:06 +0300

    Changed the page's mapping to "/course"

--- src/main/java/com/sumerge/springtask/controller/CourseController.java
+++ src/main/java/com/sumerge/springtask/controller/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/course")
 public class CourseController {
 
     private final CourseService courseService;
