f093730df81b831596d769a212ef71506ab3260e
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-06 16:55:40 +0300

    Fixed DTO

--- src/main/java/com/sumerge/springtask/DTOs/CourseDTO.java
+++ src/main/java/com/sumerge/springtask/DTOs/CourseDTO.java
@@ -1,12 +1,8 @@
 package com.sumerge.springtask.DTOs;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+
 public class CourseDTO {
 
     @NotNull
@@ -14,4 +10,36 @@ public class CourseDTO {
     private String course_description;
     @NotNull
     private int course_credit;
+
+
+    public CourseDTO(String course_name, String course_description, int course_credit) {
+        this.course_name = course_name;
+        this.course_description = course_description;
+        this.course_credit = course_credit;
+    }
+
+    public @NotNull String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(@NotNull String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getCourse_description() {
+        return course_description;
+    }
+
+    public void setCourse_description(String course_description) {
+        this.course_description = course_description;
+    }
+
+    @NotNull
+    public int getCourse_credit() {
+        return course_credit;
+    }
+
+    public void setCourse_credit(@NotNull int course_credit) {
+        this.course_credit = course_credit;
+    }
 }
