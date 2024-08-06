1f0ed657d3d98d7174cf6c0e5139986a98e45dfb
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-06 16:55:58 +0300

    Fixed Controller to work correctly

--- src/main/java/com/sumerge/springtask/controller/CourseController.java
+++ src/main/java/com/sumerge/springtask/controller/CourseController.java
@@ -1,16 +1,16 @@
 package com.sumerge.springtask.controller;
 
-import com.sumerge.springtask.model.Course;
+import com.sumerge.springtask.DTOs.CourseDTO;
 import com.sumerge.springtask.service.CourseService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 
 @RestController
+@RequestMapping("/api")
 public class CourseController {
 
     private final CourseService courseService;
@@ -26,45 +26,38 @@ public class CourseController {
     }
 
     @GetMapping("/view/{id}")
-    public String viewById(@PathVariable int id) {
-        try {
-            return courseService.findById(id).toString();
-        }
-        catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found!");
-        }
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "404", description = "Course ID not found")
+    })
+    public CourseDTO viewById(@PathVariable Long id) {
+            return courseService.findById(id);
     }
 
     @PostMapping("/add")
-    public void add(@RequestBody Course course) {
-        courseService.save(course);
+    public void add(@RequestBody CourseDTO courseDTO) {
+        courseService.save(courseDTO);
     }
 
     @PutMapping("/update/{id}")
-    public void update(@PathVariable int id, @RequestBody Course updatedCourse) {
-        try {
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Course updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "404", description = "Course ID not found")
+    })
+    public void update(@PathVariable Long id, @RequestBody CourseDTO updatedCourse) {
             courseService.update(id, updatedCourse);
-        }
-        catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found!");
-        }
     }
 
 
     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable int id) {
-        try {
+    public void delete(@PathVariable Long id) {
             courseService.delete(id);
-        }
-        catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found!");
-        }
     }
 
-    @GetMapping("/discover")
-    public String recommendedCourses() {
-        List<Course> courses = courseService.recommendCourse();
-        return courses.toString();
+    @GetMapping("/discover/page={page}&size={size}")
+    public Page<CourseDTO> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        return courseService.findAll(page, size);
     }
 
 }
