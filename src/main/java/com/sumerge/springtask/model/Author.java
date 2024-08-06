55e6797c4336d3d06c8b2277e862985966788024
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-06 16:56:13 +0300

    Fixed Course & Author

--- src/main/java/com/sumerge/springtask/model/Author.java
+++ src/main/java/com/sumerge/springtask/model/Author.java
@@ -1,22 +1,62 @@
 package com.sumerge.springtask.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.util.Date;
 
-@Getter
-@Setter
+
 @Entity
 @Table(name = "Author")
 public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long author_id;
+    private Long id;
     private String author_name;
     private String author_email;
     private Date author_birthdate;
 
+    public Author(Long id, String author_name, String author_email, Date author_birthdate) {
+        this.id = id;
+        this.author_name = author_name;
+        this.author_email = author_email;
+        this.author_birthdate = author_birthdate;
+    }
+
+    public Author() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public String getAuthor_email() {
+        return author_email;
+    }
+
+    public void setAuthor_email(String author_email) {
+        this.author_email = author_email;
+    }
+
+    public Date getAuthor_birthdate() {
+        return author_birthdate;
+    }
+
+    public void setAuthor_birthdate(Date author_birthdate) {
+        this.author_birthdate = author_birthdate;
+    }
 }
--- src/main/java/com/sumerge/springtask/model/Course.java
+++ src/main/java/com/sumerge/springtask/model/Course.java
@@ -1,22 +1,58 @@
 package com.sumerge.springtask.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
-@Getter
-@Setter
+
 @Entity
 @Table(name ="Course")
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long course_id;
+    private Long id;
     private String course_name;
     private String course_description;
     private int course_credit;
 
+    public Course(Long id, String course_name, String course_description, int course_credit) {
+        this.id = id;
+        this.course_name = course_name;
+        this.course_description = course_description;
+        this.course_credit = course_credit;
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getCourse_description() {
+        return course_description;
+    }
+
+    public void setCourse_description(String course_description) {
+        this.course_description = course_description;
+    }
+
+    public int getCourse_credit() {
+        return course_credit;
+    }
+
+    public void setCourse_credit(int course_credit) {
+        this.course_credit = course_credit;
+    }
 }
