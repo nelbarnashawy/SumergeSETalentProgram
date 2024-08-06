d3df8fbf6ba64c7c8c3d1018c900f700a33693b4
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-06 16:54:34 +0300

    Fixed CourseService and made it implement JPA

--- src/main/java/com/sumerge/springtask/service/CourseService.java
+++ src/main/java/com/sumerge/springtask/service/CourseService.java
@@ -1,64 +1,82 @@
 package com.sumerge.springtask.service;
 
-import com.sumerge.springtask.CourseRecommender;
+import com.sumerge.springtask.DTOs.CourseDTO;
+import com.sumerge.springtask.mappers.CourseMapper;
 import com.sumerge.springtask.model.Course;
 import com.sumerge.springtask.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 
 @Service
-public class CourseService implements CourseRepository{
+public class CourseService {
 
     private CourseRepository courseRepository;
-    private CourseRecommender courseRecommender;
+    //private CourseRecommender courseRecommender;
 
-    public CourseRepository getCourseRepository() {
-        return courseRepository;
+    private final CourseMapper courseMapper;
+
+    @Autowired
+    public CourseService(CourseMapper courseMapper){
+        this.courseMapper = courseMapper;
     }
 
     @Autowired
-    public void setCourseRepository(@Qualifier("courseRepo") CourseRepository courseRepository) {
+    public void setCourseRepository(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
 
-    public CourseRecommender getCourseRecommender() {
-        return courseRecommender;
+//    public CourseRecommender getCourseRecommender() {
+//        return courseRecommender;
+//    }
+//
+//    @Autowired
+//    public void setCourseRecommender(CourseRecommender courseRecommender) {
+//        this.courseRecommender = courseRecommender;
+//    }
+
+//    public List<Course> recommendCourse(){
+//        return courseRepository.findAll();
+//    }
+
+    public CourseDTO getCourseDTO(Course course){
+        return courseMapper.CoursetoCourseDTO(course);
     }
 
-    @Autowired
-    public void setCourseRecommender(CourseRecommender courseRecommender) {
-        this.courseRecommender = courseRecommender;
+    public Course getCourse(CourseDTO courseDTO){
+        return courseMapper.CourseDTOtoCourse(courseDTO);
     }
 
-    public List<Course> recommendCourse(){
-        return courseRepository.findAll();
+    public CourseDTO findById(Long id) {
+        Course course = courseRepository.findFirstById(id);
+        return getCourseDTO(course);
     }
 
-    @Override
-    public Course findById(int id) {
-        return courseRepository.findById(id);
+    public Page<CourseDTO> findAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Course> courses = courseRepository.findAll(pageable);
+        return courses.map(this::getCourseDTO);
     }
 
-    @Override
-    public List<Course> findAll() {
-        return courseRepository.findAll();
-    }
-
-    @Override
-    public void save(Course course) {
+    public void save(CourseDTO courseDTO) {
+        Course course = getCourse(courseDTO);
         courseRepository.save(course);
     }
 
-    @Override
-    public void delete(int id) {
-        courseRepository.delete(id);
+    public void delete(Long id) {
+        courseRepository.deleteById(id);
     }
 
-    @Override
-    public void update(int id, Course course) {
-        courseRepository.update(id, course);
+    public void update(Long id, CourseDTO courseDTO) {
+        Course course = getCourse(courseDTO);
+        Course toUpdate = courseRepository.findFirstById(id);
+        toUpdate.setCourse_name(course.getCourse_name());
+        toUpdate.setCourse_description(course.getCourse_description());
+        toUpdate.setCourse_credit(course.getCourse_credit());
+        courseRepository.save(toUpdate);
     }
 }
