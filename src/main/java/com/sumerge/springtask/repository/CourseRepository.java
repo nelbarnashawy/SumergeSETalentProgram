25d14c511e6992f36cc2f49e8d1e3ff846828da4
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-07 16:28:26 +0300

    Added @Repository to my repositories

--- src/main/java/com/sumerge/springtask/repository/CourseRepository.java
+++ src/main/java/com/sumerge/springtask/repository/CourseRepository.java
@@ -2,7 +2,9 @@ package com.sumerge.springtask.repository;
 
 import com.sumerge.springtask.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     Course findFirstById(Long id);
 }
