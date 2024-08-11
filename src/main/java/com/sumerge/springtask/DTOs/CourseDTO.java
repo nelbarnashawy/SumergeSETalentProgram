fdd4e99e356618af0be19a78e93fbabac49347c4
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-11 14:23:04 +0300

    Added NoArgsConstructor in CourseDTO in order to be able to test and use setters and getters

--- src/main/java/com/sumerge/springtask/DTOs/CourseDTO.java
+++ src/main/java/com/sumerge/springtask/DTOs/CourseDTO.java
@@ -3,17 +3,18 @@ package com.sumerge.springtask.DTOs;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CourseDTO {
 
     @NotNull(message = "Name is required")
     private String courseName;
     private String courseDescription;
-    @NotNull(message = "Credit is required")
     private int courseCredit;
 
 }
