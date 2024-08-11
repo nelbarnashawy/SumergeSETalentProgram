51bc2e71dd056226779772030edf217693cde767
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-11 10:20:33 +0300

    Removed @AllArgsConstructor as it's never used

--- src/main/java/com/sumerge/springtask/model/Author.java
+++ src/main/java/com/sumerge/springtask/model/Author.java
@@ -12,7 +12,6 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "Author")
 public class Author {
--- src/main/java/com/sumerge/springtask/model/Course.java
+++ src/main/java/com/sumerge/springtask/model/Course.java
@@ -12,7 +12,6 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name ="Course")
 public class Course {
