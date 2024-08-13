62b224d4a4d490ff7fa9d9a32b58618b8be02128
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-13 17:07:18 +0300

    Fixed issue with all non-GET requests not getting authenticated.

--- src/main/java/com/sumerge/springtask/security/SecurityConfig.java
+++ src/main/java/com/sumerge/springtask/security/SecurityConfig.java
@@ -10,11 +10,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,13 +21,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .addFilterBefore(new HeaderFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests((authorize) ->
                         authorize
-                                .requestMatchers("/courses/view/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/courses/view/**").permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
-
         return http.build();
     }
 
