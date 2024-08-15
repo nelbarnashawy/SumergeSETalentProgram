8f892c577f5592f32c6413d1d4878ff4f0eaeed6
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-15 15:21:49 +0300

    Removed unused imports

--- src/main/java/com/sumerge/springtask/security/SecurityConfig.java
+++ src/main/java/com/sumerge/springtask/security/SecurityConfig.java
@@ -8,11 +8,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
