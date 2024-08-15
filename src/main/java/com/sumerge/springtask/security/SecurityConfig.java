e0ab8ed095dd468ec52dcc32efbc545f758dd833
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-15 16:26:11 +0300

    Changed some Security Settings

--- src/main/java/com/sumerge/springtask/security/SecurityConfig.java
+++ src/main/java/com/sumerge/springtask/security/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,12 +24,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .addFilterBefore(new HeaderFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new HeaderFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((authorize) ->
                         authorize
                                 .requestMatchers(HttpMethod.GET, "/courses/view/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/authors/authorByEmail/**").permitAll()
-                                .anyRequest().hasAnyRole("ADMIN", "USER")
+                                .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults());
