f89c376dd1f8c3c55d46e4e064564a297eb0410c
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-15 16:27:13 +0300

    Fixed AuthorController unit test!

--- src/test/java/com/sumerge/springtask/controller/AuthorControllerTest.java
+++ src/test/java/com/sumerge/springtask/controller/AuthorControllerTest.java
@@ -8,7 +8,8 @@ import com.sumerge.springtask.service.AuthorService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -21,7 +22,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = AuthorController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class AuthorControllerTest {
 
     @MockBean
