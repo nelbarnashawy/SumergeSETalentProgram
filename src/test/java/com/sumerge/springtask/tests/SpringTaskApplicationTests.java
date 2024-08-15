37e4898deb6372ab2029b78b873bad914c7dc870
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-15 12:44:39 +0300

    Created the first successfully working test!

--- src/test/java/com/sumerge/springtask/tests/SpringTaskApplicationTests.java
+++ src/test/java/com/sumerge/springtask/tests/SpringTaskApplicationTests.java
@@ -2,13 +2,18 @@ package com.sumerge.springtask.tests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sumerge.springtask.controller.CourseController;
+import com.sumerge.springtask.dto.CourseDTO;
+import com.sumerge.springtask.mapper.CourseMapper;
 import com.sumerge.springtask.model.Course;
+import com.sumerge.springtask.repository.CourseRepository;
+import com.sumerge.springtask.service.CourseService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -21,37 +26,40 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class SpringTaskApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private CourseController courseController;
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private CourseMapper courseMapper;
 
-	private ObjectMapper objectMapper;
+    @BeforeEach
+    void setUp() {
+    }
 
-	@BeforeEach
-	void setUp() {
-		this.mockMvc = MockMvcBuilders
-				.standaloneSetup(CourseController.class)
-				.build();
-		this.objectMapper = new ObjectMapper();
-	}
+    @Test
+    @WithMockUser(username = "testAdmin", password = "testAdmin", roles = {"ADMIN"})
+    void addNewCourse() throws Exception {
+        // ARRANGE
+        CourseDTO courseDTO = new CourseDTO();
+        courseDTO.setCourseName("Math");
+        courseDTO.setCourseDescription("Math Test");
+        courseDTO.setCourseCredit(8);
+        // ACT
+        // ASSERT
+        mockMvc.perform(post("/courses/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(courseDTO))
+                        .header("x-validation-report", true))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Course added successfully"));
 
-	@Test
-	void addNewCourse() throws  Exception{
-		// ARRANGE
-		Course course = new Course();
-		course.setCourseName("Math");
-		course.setCourseDescription("Math Test");
-		course.setCourseCredit(8);
-		// ACT
-		// ASSERT
-		mockMvc.perform(post("/courses/add")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(course)))
-				.andExpect(status().isOk())
-				.andExpect(content().string("Course added successfully"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.courseName").value("Math"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.courseDescription").value("Math Test"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.courseCredit").value(8));
-
-	}
+    }
 
 }
